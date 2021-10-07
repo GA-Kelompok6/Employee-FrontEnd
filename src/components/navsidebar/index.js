b332ee6aa959497874d7ed077576a7d7c27e50f7
shaafiraks <shafiraaks@gmail.com>
2021-10-07 14:51:37 +0800

    Update index.js

--- src/components/navsidebar/index.js
+++ src/components/navsidebar/index.js
@@ -8,67 +8,67 @@ import { userData } from "./UserData";
 import { IconContext } from "react-icons";
 
 function Navbar() {
-   const [sidebar, setSidebar] = useState(false);
-   const [userbar, setUserbar] = useState(false);
-   const showSidebar = () => setSidebar(!sidebar);
-   const showUserbar = () => setUserbar(!userbar);
-   require("./Navbar.css");
-   return (
-      <>
-         <IconContext.Provider value={{ color: "#fff" }}>
-            <div className="navbar">
-               <Link to="#" className="menu-bars">
-                  <FaIcons.FaBars onClick={showSidebar} />
-               </Link>
-               <Link to="#" className="menu-bars" style={{ paddingRight: "30px" }}>
-                  <FaIcons.FaUser onClick={showUserbar} />
-               </Link>
-            </div>
+  const [sidebar, setSidebar] = useState(false);
+  const [userbar, setUserbar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+  const showUserbar = () => setUserbar(!userbar);
+  require("./Navbar.css");
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          <Link to="#" className="menu-bars">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+          <Link to="#" className="menu-bars" style={{ paddingRight: "30px" }}>
+            <FaIcons.FaUser onClick={showUserbar} />
+          </Link>
+        </div>
 
-            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-               <ul className="nav-menu-items" onClick={showSidebar}>
-                  <li className="navbar-toggle">
-                     <Link to="#" className="menu-bars">
-                        <AiIcons.AiOutlineClose />
-                     </Link>
-                  </li>
-                  {SidebarData.map((item, index) => {
-                     return (
-                        <li key={index} className={item.cName}>
-                           <Link to={item.path}>
-                              {item.icon}
-                              <span className="spanSidebar">{item.title}</span>
-                           </Link>
-                        </li>
-                     );
-                  })}
-               </ul>
-            </nav>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span className="spanSidebar">{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
 
-            <nav className={userbar ? "user-menu active" : "user-menu"}>
-               <ul className="nav-menu-items" onClick={showUserbar}>
-                  <li className="navbar-toggle">
-                     <Link to="#" className="menu-bars-right">
-                        <AiIcons.AiOutlineClose />
-                     </Link>
-                  </li>
-                  {userData.map((item, index) => {
-                     // Caranya gimanaaaa???
-                     // window.localStorage.clear();
-                     return (
-                        <li key={index} className={item.cName}>
-                           <Link to={item.path}>
-                              {item.icon}
-                              <span className="spanSidebar">{item.title}</span>
-                           </Link>
-                        </li>
-                     );
-                  })}
-               </ul>
-            </nav>
-         </IconContext.Provider>
-      </>
-   );
+        <nav className={userbar ? "user-menu active" : "user-menu"}>
+          <ul className="nav-menu-items" onClick={showUserbar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars-right">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {userData.map((item, index) => {
+              // Caranya gimanaaaa???
+              // window.localStorage.clear();
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span className="spanSidebar">{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
 }
 
 export default Navbar;
