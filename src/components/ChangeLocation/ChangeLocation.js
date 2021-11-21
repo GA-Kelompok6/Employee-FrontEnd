19d49c38c0c40adb2695d62ea20709d0330c5b07
JRSKelvin <74870436+JRSKelvin@users.noreply.github.com>
2021-11-21 15:40:45 +0700

    Fixing React Map GL

--- src/components/ChangeLocation/ChangeLocation.js
+++ src/components/ChangeLocation/ChangeLocation.js
@@ -5,7 +5,6 @@ import Location from "./index"
 import MapGL, {Marker, NavigationControl} from 'react-map-gl';
 import Pin from './pin';
 import ControlPanel from './control-panel';
-import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function ChangeLocation({ Location }) {
    require("./style.css")
